fdc19a96c8b84db59737825e96176e008c1b5e8c
Seb James <sebj_coder3@pm.me>
2025-07-09 17:45:33 +0100

    Zernike examples

---
+++ examples/zernike.cpp
@@ -0,0 +1,61 @@
+// Demo Zernike polynomials
+
+#include <format>
+#include <complex>
+#include <sm/mathconst>
+#include <sm/algo>
+#include <sm/vvec>
+#include <mplot/Visual.h>
+#include <mplot/PolarVisual.h>
+
+int main()
+{
+    // You can require that n - |m| is even to "ensure the rotational invariant property is met"
+    // (see https://doi.org/10.7554/eLife.54026, p 18)
+    constexpr bool require_n_minus_abs_m_even = true;
+
+    using mc = sm::mathconst<double>;
+
+    mplot::Visual<> v (1024, 768, "Zernike Polynomials");
+
+    constexpr std::size_t N = 20;
+    sm::vvec<double> rho;
+    rho.linspace (0.0, 1.0, N);
+
+    sm::vvec<double> theta;
+    theta.linspace (0.0, mc::two_pi, N);
+
+    for (unsigned int n = 0; n < 10; ++n) {
+
+        for (int m = 0; m <= static_cast<int>(n); ++m) {
+
+            if constexpr (require_n_minus_abs_m_even) { if ((n - std::abs(m)) % 2 != 0) { continue; } }
+
+            auto pv = std::make_unique<mplot::PolarVisual<double>> (sm::vec<float>{1.4f * n, 1.4f * m, 0});
+            v.bindmodel (pv);
+            pv->cm.setType (mplot::ColourMapType::Cork);
+            pv->setFrameTextColour (mplot::colour::white);
+            pv->radius = 3.5f;
+            pv->tf.fontsize = 0.4f;
+            pv->numrings = N;
+            pv->numsegs = N;
+            pv->twodimensional = false;
+
+            sm::vvec<double> Vnm_real;
+            for (auto rh : rho) {
+                double r_nm = sm::algo::zern_radial_poly (n, m, rh);
+                for (auto th : theta) {
+                    Vnm_real.push_back (std::real(sm::algo::zern_polynomial (m, r_nm, th)));
+                }
+            }
+
+            pv->setScalarData (&Vnm_real);
+
+            pv->finalize();
+            v.addVisualModel (pv);
+        }
+
+    }
+
+    v.keepOpen();
+}
---
+++ mplot/PolarVisual.h
@@ -0,0 +1,174 @@
+/*
+ * A visual for polar plots (rho, theta)
+ */
+
+#pragma once
+
+#include <sm/mathconst>
+#include <sm/vec>
+
+#include <mplot/VisualDataModel.h>
+#include <mplot/GraphVisual.h>
+
+namespace mplot {
+
+    template <typename F, int glver = mplot::gl::version_4_1>
+    struct PolarVisual : public VisualDataModel<F, glver>
+    {
+        using mc = sm::mathconst<F>;
+
+        //! Constructor
+        //! \param _offset The offset within mplot::Visual space to place this model
+        PolarVisual (const sm::vec<float> _offset)
+        {
+            this->mv_offset = _offset;
+            this->viewmatrix.translate (this->mv_offset);
+            // Set default text features
+            this->tf.fontsize = 0.05f;
+            this->tf.fontres = 48;
+            this->tf.colour = this->framecolour;
+            // Like graphs, Polar visuals don't rotate by default. If you want yours to, set this false in your client code.
+            this->twodimensional = true;
+        }
+
+        void setTextColour (const std::array<float, 3>& c) { this->tf.colour = c; }
+        void setFrameColour (const std::array<float, 3>& c) { this->framecolour = c; }
+        void setFrameTextColour (const std::array<float, 3>& c) { this->framecolour = this->tf.colour = c; }
+
+        void initializeVertices()
+        {
+            this->determine_datasize();
+            if (this->datasize == 0) { return; }
+
+            this->setupScaling();
+
+            // Auto-set ticklabelgap
+            auto em = this->makeVisualTextModel (this->tf);
+            mplot::TextGeometry em_geom = em->getTextGeometry (std::string("m"));
+            this->ticklabelgap = em_geom.width() / 2.0f;
+            this->drawFrame();
+            this->drawTickLabels();
+            this->fillFrameWithColour();
+        }
+
+        //! Draw a circular frame around the polar plot
+        void drawFrame()
+        {
+            // Draw an approximation of a circle.
+            this->computeFlatCircleLine (sm::vec<float>{0,0,this->z}, this->uz, this->radius + this->framelinewidth/2.0f,
+                                         this->framelinewidth, this->framecolour, this->numsegs);
+        }
+
+        //! Draw the tick labels (the numbers or whatever text the client code has given us)
+        void drawTickLabels()
+        {
+            // Reset these members
+            this->ticklabelheight = 0.0f;
+            this->ticklabelwidth = 0.0f;
+
+            if (this->label_angles.empty()) {
+                // Auto-fill label_angles based on labels size.
+                // example order for 4: mc::pi_over_2, mc::pi, mc::three_pi_over_2, 0.0f
+                this->label_angles.resize (this->labels.size());
+                for (unsigned int i = 0; i < this->labels.size(); ++i) {
+                    // North is pi/2, so that's the start:
+                    this->label_angles[i] = mc::pi_over_2 + i * (mc::two_pi / this->labels.size());
+                    // Rescale any that exceed 2pi:
+                    this->label_angles[i] = this->label_angles[i] < F{0} ? this->label_angles[i] + mc::two_pi : this->label_angles[i];
+                    this->label_angles[i] = this->label_angles[i] > mc::two_pi ? this->label_angles[i] - mc::two_pi : this->label_angles[i];
+                }
+            }
+
+            for (unsigned int i = 0; i < this->label_angles.size(); ++i) {
+                std::string s = this->labels[i];
+                auto lbl = this->makeVisualTextModel (this->tf);
+                mplot::TextGeometry geom = lbl->getTextGeometry (s);
+                this->ticklabelheight = geom.height() > this->ticklabelheight ? geom.height() : this->ticklabelheight;
+                this->ticklabelwidth = geom.width() > this->ticklabelwidth ? geom.width() : this->ticklabelwidth;
+                // Dep. on angle, the additional gap for the text will need to be based on different aspects of the text geometry
+                float geom_gap = std::abs(std::cos(label_angles[i]) * geom.half_width()) + std::abs(std::sin(label_angles[i]) * geom.half_height());
+                float lbl_r = this->radius + this->framelinewidth + this->ticklabelgap + geom_gap;
+                sm::vec<float> lblpos = {
+                    lbl_r * std::cos (static_cast<float>(label_angles[i])) - geom.half_width(),
+                    lbl_r * std::sin (static_cast<float>(label_angles[i])) - geom.half_height(),
+                    this->z
+                };
+                lbl->setupText (s, lblpos+this->mv_offset, this->tf.colour);
+                this->texts.push_back (std::move(lbl));
+            }
+        }
+
+        // Draw the Polar plot colour
+        void fillFrameWithColour()
+        {
+            // Check numrings * numsegs is same size as scalar or vector data
+            if (this->numrings * this->numsegs != this->datasize) {
+                throw std::runtime_error ("Uh oh");
+            }
+
+            sm::vec<float> centre = {0,0,this->z};
+
+            for (int ring = this->numrings; ring > 0; ring--) {
+
+                float r_out = this->radius * static_cast<float>(ring)/this->numrings;
+                float r_in = this->radius * static_cast<float>(ring-1)/this->numrings;
+
+                for (int j = 0; j < static_cast<int>(this->numsegs); j++) {
+
+                    std::array<float, 3> clr = this->setColour (ring * this->numsegs + j);
+
+                    float t = j * sm::mathconst<float>::two_pi/static_cast<float>(this->numsegs);
+                    sm::vec<float> c_in = this->uy * sin(t) * r_in + this->ux * cos(t) * r_in;
+                    this->vertex_push (centre+c_in, this->vertexPositions);
+                    this->vertex_push (this->uz, this->vertexNormals);
+                    this->vertex_push (clr, this->vertexColors);
+                    sm::vec<float> c_out = this->uy * sin(t) * r_out + this->ux * cos(t) * r_out;
+                    this->vertex_push (centre+c_out, this->vertexPositions);
+                    this->vertex_push (this->uz, this->vertexNormals);
+                    this->vertex_push (clr, this->vertexColors);
+                }
+                // Added 2*segments vertices to vertexPositions
+
+                for (int j = 0; j < static_cast<int>(this->numsegs); j++) {
+                    int jn = (numsegs + ((j+1) % numsegs)) % numsegs;
+                    this->indices.push_back (this->idx+(2*j));
+                    this->indices.push_back (this->idx+(2*jn));
+                    this->indices.push_back (this->idx+(2*jn+1));
+                    this->indices.push_back (this->idx+(2*j));
+                    this->indices.push_back (this->idx+(2*jn+1));
+                    this->indices.push_back (this->idx+(2*j+1));
+                }
+                this->idx += 2 * this->numsegs; // nverts
+            }
+        }
+
+        //! The ColourMap to show (copy in). Should be type ColourMapType::HSV
+        mplot::ColourMap<F> cm;
+        //! The radius of the HSVwheel
+        float radius = 1.0f;
+        //! Position in z in model space. Default is just 0.
+        float z = 0.0f;
+        //! colour for the axis box/lines. Text colour is in TextFeatures tf.colour
+        std::array<float, 3> framecolour = mplot::colour::black;
+        //! The line width of the frame
+        float framelinewidth = 0.01f;
+        //! The label strings  that should be displayed. Order the elements anti-clockwise, starting from the 'north' element.
+        std::deque<std::string> labels = {"N", "W", "S", "E"};
+        //! The positions, as angles for the labels. If empty, these will be auto-computed
+        std::deque<F> label_angles = { /*mc::pi_over_2, mc::pi, mc::three_pi_over_2, 0.0f*/ };
+        // Stores all the text features for this ColourBar (font, colour, font size, font res)
+        mplot::TextFeatures tf;
+        //! Gap to x axis tick labels. Gets auto-set
+        float ticklabelgap = 0.05f;
+        //! The number of segments to make in each ring of the colourmap fill. Depends on your data.
+        unsigned int numsegs = 128;
+        //! How many rings of colour? Depends on your data
+        unsigned int numrings = 64;
+    protected:
+        //! tick label height
+        float ticklabelheight = 0.0f;
+        //! tick label width
+        float ticklabelwidth = 0.0f;
+    };
+
+} // namespace
