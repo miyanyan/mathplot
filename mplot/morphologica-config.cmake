cdb5391244711186d8db434e2ffde4553c8a83b5
Seb James <sebj_coder3@pm.me>
2025-07-11 10:44:44 +0100

    Removes stale file

--- mplot/morphologica-config.cmake
+++
@@ -1,3 +0,0 @@
-# morphologica-config.cmake - package configuration file
-get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-include(${SELF_DIR}/${CMAKE_BUILD_TYPE}/morphologica.cmake)
